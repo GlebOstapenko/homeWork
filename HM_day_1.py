9e5e0d0ecc0f24d29d73eeb0f3538c0383ce9cda
Gleb Ostapenko <gleb.ostapenko1999@gmail.com>
2022-04-07 17:28:31 +0300

    first HW - final

--- HM_day_1.py
+++ HM_day_1.py
@@ -1,4 +1,4 @@
-def get_num(word):
+def get_num_float(word):
     while True:
         number = input(word)
         try:
@@ -6,10 +6,18 @@ def get_num(word):
         except:
             print("Ошибка, пробуй ещё раз ввести число")
 
+def get_num_int(word):
+    while True:
+        number = input(word)
+        try:
+            return (int(number))
+        except:
+            print("Ошибка, пробуй ещё раз ввести число")
+
 # Задача 1: Створіть дві змінні first=10, second=30.
 # Виведіть на екран результат математичної взаємодії (+, -, *, / и тд.) для цих чисел.
-first_number = get_num("Укажите первое число: ")
-second_number = get_num("Укажите второе число: ")
+first_number = get_num_float("Укажите первое число: ")
+second_number = get_num_float("Укажите второе число: ")
 print("Задача 1")
 print(f"{first_number} + {second_number} = {first_number + second_number}")
 print(f"{first_number} - {second_number} = {first_number - second_number}")
@@ -34,7 +42,7 @@ print(f"{first_number} != {second_number} - {task_number}")
 # Задача 3: Створіть змінну - результат конкатенації (складання) строк str1="Hello " и str2="world".
 # Виведіть на екран.
 print("\nЗадача 3")
-count_word = int(input("Укажите количество слов в предложении: "))
+count_word = get_num_int("Укажите количество слов в предложении: ")
 sentence = ""
 for i in range(1, count_word + 1):
     word = input(f"Введите слово №{i}: ")
