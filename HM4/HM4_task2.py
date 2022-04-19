4e8ab6e8e2d613ac19c49e99dba1a466c7aad20b
Gleb Ostapenko <gleb.ostapenko1999@gmail.com>
2022-04-19 13:56:31 +0300

    HW3

--- HM4/HM4_task2.py
+++ HM4/HM4_task2.py
@@ -16,16 +16,20 @@ task_dict = {"cito": 47.999,
              "the_partner": 38.988,
              "sota": 37.720,
              "rozetka": 38.003}
-
-task_min = all_func.get_any_type("Введите минимальную сумму: ", "float")
 while True:
-    task_max = all_func.get_any_type("Введите максимальную сумму: ", "float")
+    task_min = all_func.get_any_type("Введіть мінімальну суму: ", "float")
+    if task_min<0:
+        print("Негативної суми не може бути")
+        continue
+    break
+while True:
+    task_max = all_func.get_any_type("Введіть максимальну суму: ", "float")
     if task_max < task_min:
-        print("Сумма меньше минимальной, попробуй ещё")
+        print("Сума менша за мінімальну, спробуй ще")
         continue
     break
 res_lst = []
 for shop in task_dict:
     if task_dict[shop] >= task_min and task_dict[shop] <= task_max:
         res_lst.append(shop)
-print(f"Список магазинов которые подходят: {res_lst}" )
+print(f"Список магазинів, які підходять: {res_lst}" )
