25ee1c2139bed7c2365e807c587d5f950b5412f3
Gleb Ostapenko <gleb.ostapenko1999@gmail.com>
2022-05-12 09:26:55 +0300

    HW9

--- HM9/HM9_task2.py
+++ HM9/HM9_task2.py
@@ -1,19 +1,23 @@
 def args_to_str(func):
-    def replacing_with_string(*args,**kwargs):
+    def replacing_with_string(*args, **kwargs):
         args_string = []
         for arg in args:
             args_string.append(str(arg))
         for kwarg in kwargs:
             args_string.append(str(kwargs[kwarg]))
 
-        return func(args_string)
+        return str(func(args_string))
+
     return replacing_with_string
 
 
 @args_to_str
-def get_str(args):
-    result_str = " ".join(args)
-    return result_str
+def get_tuple(args):
+    for i in range(len(args)):
+        if "4" in args[i]:
+                args[i]="@$@"
+    return tuple(args)
 
 
-print(get_str(1,2,"52",4,"78",8, c=2, d=4))
+print(get_tuple(1, 2, "52", 4, "78", 8, c=2, d=4))
+print(type(get_tuple(1, 2, "52", 4, "78", 8, c=2, d=4)))
