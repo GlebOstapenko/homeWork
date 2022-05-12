54066098952e3e284fafd9fe5b92281a319d3574
Gleb Ostapenko <gleb.ostapenko1999@gmail.com>
2022-05-12 08:53:33 +0300

    HW9

--- HM9/HM9_task2.py
+++ HM9/HM9_task2.py
@@ -1,11 +1,16 @@
-def get_type(arg_type):
-    def decorator(func):
-        def wrap(arg):
-            return arg_type(func(arg_type(arg)))
-        return wrap
-    return decorator
+def args_to_str(func):
+    def replacing_with_string(*args):
+        args_string = []
+        for arg in args:
+            args_string.append(str(arg))
+        return func(args_string)
+    return replacing_with_string
 
-@get_type(str)
-def get_str_len(my_str):
-    return len(my_str)
 
+@args_to_str
+def get_str(args):
+    result_str = " ".join(args)
+    return result_str
+
+
+print(get_str(1,2,"52",4,"78",8))
