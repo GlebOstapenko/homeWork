6163f62a4b6a48d3b2b2f58de364bbcaf796d84d
Gleb Ostapenko <gleb.ostapenko1999@gmail.com>
2022-05-23 00:30:51 +0300

    HW9

--- HM12/HM12_class_list.py
+++ HM12/HM12_class_list.py
@@ -1,24 +1,30 @@
 from abc import ABC, abstractmethod
+import inspect
 
 
 class Vehicle(ABC):
-    mark = ""
-    model = ""
-    max_speed = 0
-    fuel_consumption = ""
-    maximum_load = ""
-    volume_fuel_tank = ""
-    number_passenger = 0
+    __slots__ = (
+        "mark", "model", "max_speed", "fuel_consumption", "maximum_load", "volume_fuel_tank", "number_passenger")
 
-    @abstractmethod
-    def __init__(self, create_information):
-        self.mark = create_information["mark"]
-        self.model = create_information["model"]
-        self.max_speed = create_information["max_speed"]
-        self.fuel_consumption = create_information["fuel_consumption"]
-        self.maximum_load_capacity = create_information["maximum_load"]
-        self.volume_tank = create_information["volume_fuel_tank"]
-        self.number_passenger = create_information["number_passenger"]
+    def __init__(self, mark, model, **kwargs):
+        self.mark = mark
+        self.model = model
+
+        for param, value in kwargs.items():
+
+            try:
+                command_exec = f"self.{param} = {value}"
+                exec(command_exec)
+            except NameError:
+                print(f"{param} - може бути лише числовим значенням, [{value}] не підходить")
+            except AttributeError:
+                print(f"Параметр {param} не в списку допустимих параметрів")
+
+    def __call__(self):
+        for i in inspect.getmembers(self):
+            if not i[0].startswith('_'):
+                if not inspect.ismethod(i[1]):
+                    print(f"{i[0]} -> {i[1]}")
 
     @abstractmethod
     def start_sound(self):
@@ -26,51 +32,21 @@ class Vehicle(ABC):
 
 
 class Car(Vehicle):
-    engine_capacity = 0
-    number_wheels = 0
-    count_car = 0
-
-    def __init__(self, create_information):
-        super(Car, self).__init__(create_information)
-        self.engine_capacity = create_information["engine_capacity"]
-        self.number_wheels = create_information["number_wheels"]
-        Car.count_car += 1
-
-    def __call__(self, *args, **kwargs):
-        print(self.mark)
-        print(self.model)
+    __slots__ = "number_wheels", "engine_capacity"
 
     def start_sound(self):
         print("Врум")
 
 
 class Plane(Vehicle):
-    number_engines = 0
-    maximum_flight_altitude = ""
-    minimum_flight_altitude = ""
-    count_plane = 0
-
-    def __init__(self, create_information):
-        super(Plane, self).__init__(create_information)
-        self.number_engines = create_information["number_engines"]
-        self.maximum_flight_altitude = create_information["maximum_flight_altitude"]
-        self.minimum_flight_altitude = create_information["minimum_flight_altitude"]
-        Plane.count_plane += 1
+    __slots__ = "number_engines", "maximum_flight_altitude", "minimum_flight_altitude"
 
     def start_sound(self):
         print("Виуууу")
 
 
 class Ship(Vehicle):
-    number_of_screws = 0
-    ship_type = ""
-    count_ship = 0
-
-    def __init__(self, create_information):
-        super(Ship, self).__init__(create_information)
-        self.number_of_screws = create_information["number_of_screws"]
-        self.ship_type = create_information["ship_type"]
-        Ship.count_ship += 1
+    __slots__ = "number_of_screws", "ship_type"
 
     def start_sound(self):
         print("ТуууТуууу")
--- HM12/HM12_task1.py
+++ HM12/HM12_task1.py
@@ -1,8 +1,9 @@
 from HM12_class_list import Car, Plane, Ship
-from HM12_func import creation_ship, creation_plane, creation_car
+import inspect
 
-my_car = Car(creation_car())
+my_car = Car("scds", "sdcvs", engine_capacity= 3, max_speed= "й")
+print("=================")
 my_car()
-print(Car.count_car)
-my_car2 = Car(creation_car())
-print(Car.count_car)
+my_car.start_sound()
+
+
