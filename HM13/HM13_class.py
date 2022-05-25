a91ac43a1ef75b30a2a77ce6f474159d1a5957a5
gleb.ostapenko1999@gmail.com <Razebu70749912>
2022-05-25 21:19:29 +0300

    hw13

--- HM13/HM13_class.py
+++ HM13/HM13_class.py
@@ -73,15 +73,5 @@ class Triangle:
         return self.__area
 
 
-p1 = Point(1, 5)
-p2 = Point(10, 15)
-p3 = Point(15, 15)
-l1 = Line(p1, p2)
-print(l1.length)
-l2 = Line(p2, p3)
-print(l2.length)
-l3 = Line(p3, p1)
-print(l3.length)
-tr1 = Triangle(p1, p2, p3)
-print(tr1.area)
+
 
